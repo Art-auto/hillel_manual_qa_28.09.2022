98be64792e2ce26831aa45f94ce46e0a8ffb572e
IevgenZaitsev <119672807+IevgenZaitsev@users.noreply.github.com>
2022-12-25 22:48:12 +0200

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,8 @@
+
+var lines = 5;
+var str = " ";
+var star = "*";
+for (var i = 0; i < lines; i++) {
+  str += star;
+  console.log(str);
+}
---
+++ 4.js
@@ -0,0 +1,9 @@
+
+let iteration_num = 0;
+var a=1000
+do {
+  a = a/2;
+  iteration_num++;
+  console.log(a, iteration_num);
+} while (a >50);
+
