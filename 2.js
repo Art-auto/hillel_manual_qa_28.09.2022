b325376882302483c2ca6ff771768b5a4225fe89
IevgenZaitsev <119672807+IevgenZaitsev@users.noreply.github.com>
2022-12-26 22:48:21 +0200

    Add files via upload
    изменил треугольник - файл 2, и откорректировал файл 4 по замечаниям

--- 2.js
+++ 2.js
@@ -1,8 +1,16 @@
 
-var lines = 5;
-var str = " ";
-var star = "*";
-for (var i = 0; i < lines; i++) {
-  str += star;
-  console.log(str);
+var i = 0,
+  j = 0;
+// Желаемое количество строк
+var max = 5;
+var star = "";
+
+while (i < max) {
+  star = "";
+  for (j = 0; j < max - i; j++);
+  for (j = 0; j < 1 * i + 1; j++) star += "*";
+  console.log(star);
+  i++;
 }
+
+
--- 4.js
+++ 4.js
@@ -4,6 +4,7 @@ var a=1000
 do {
   a = a/2;
   iteration_num++;
-  console.log(a, iteration_num);
-} while (a >50);
+ } while (a >=50);
+ console.log(a, iteration_num);
+ 
 
